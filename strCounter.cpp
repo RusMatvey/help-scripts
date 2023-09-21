4d2c8dc0d52e7ca926aa5507025527bce52a205d
RusMatvey <92601028+RusMatvey@users.noreply.github.com>
2023-09-21 15:18:26 +0400

    Update strCounter.cpp

--- strCounter.cpp
+++ strCounter.cpp
@@ -4,95 +4,92 @@
 
 using namespace std;
 
-vector<char> alf = { 
+struct Char{
+    char in;
+    int type;
+};
 
-'B', 'O', 'R', 'O', 'T', 'A' };
+vector<Char> alf = { {'A', 1}, {'M', 0}, {'F', 0}, {'I', 1}, {'B', 0}, {'R', 0}, {'A', 2}, {'X', 0}, {'I', 2}, {'i', 0} };
+vector<Char> dbug = { {'I', 1}, {'A', 1}, {'M', 0}, {'F', 0}, {'B', 0}, {'R', 0}, {'A', 2}, {'X', 0}, {'I', 2}, {'i', 0} };
 
-int check(string a){
+int check(vector<Char> a){
     /*if(a[7] != '0' && a[7] != '5'){
-      
-
-  return 0;
-    }
-    if(a[0] == '0'){
         return 0;
     }*/
     
-    /*for(int i = 0; i < 
-
-a.size(); ++i){
+    /*if(a[0] == '0'){
+        return 0;
+    }*/
+    
+    for(int i = 0; i < a.size(); ++i){
         for(int t = i + 1; t < a.size(); ++t){
-            if(a[i] == a[t] || 
-
-(a[i] == 'A' && a[t] == 'I') || (a[i] == 'I' && a[t] == 'A')){
+            if(a[i].type > a[t].type && a[t].type > 0){
                 return 0;
-       
-
-     }
+            }
+        }
+    }
+    
+    
+    int q_p = 0;
+    
+    for(int i = 0; i < a.size() - 1; ++i){
+        if(a[i].type > 0 && a[i + 1].type > 0){
+            ++q_p;
+        }
+    }
+    if(q_p == 0) return 0;
+    
+    
+    /*if(a[0].in == 'F' || a.back().in == 'F') return 0;
+    
+    for(int i = 1; i < a.size() - 1; ++i){
+        if(a[i].in == 'F'){
+            if(a[i - 1].in != a[i + 1].in || )
         }
     }*/
     
-    for(int i = 0; i < a.size() - 1; ++i){
-        if((a[i] == 'O' 
-
-&& a[i + 1] == 'A') || (a[i] == 'A' && a[i + 1] == 'O') || (a[i] == 'O' && a[i + 1] == 'O')){
- 
-
-           return 0;
-        }
-    }
+    
     
     return 1;
 }
 
-string str;
+vector<Char> str;
+vector<int> perm;
 
 int permut(){
-    if(str.size() == alf.size()) {
-        string toc;
-        for(auto &i : str){
-            toc.push_back(alf
-
-[i]);
+    if(perm.size() == 10) {
+        str.clear();
+        for(auto i : perm){
+            str.push_back(alf[i]);
         }
-        return check(toc);
+        return check(str);
     }
     
     int res = 0;
     
-    for(int i = 0; i < 
-
-alf.size(); ++i){
+    for(int i = 0; i < alf.size(); ++i){
         
-        for(auto t : str){
-            if(i == (int)t){
-               
-
- goto n_i;
+        for(auto t : perm){
+            if(i == t){
+                goto n_i;
             }
         }
         
-        str.push_back(char(i));
-        res += permut
-
-();
-        str.pop_back();
+        perm.push_back(i);
+        res += permut();
+        perm.pop_back();
         
         n_i:;
     }
 }
 
 int silly(){
-    if(str.size() == 6) 
-
-return check(str);
+    if(str.size() == 5) return check(str);
     int res = 0;
     
     for(auto i : alf){
         str.push_back(i);
-        
-
-res += silly();
+        res += silly();
         str.pop_back();
     }
     return res;
@@ -100,9 +97,7 @@ res += silly();
 
 int main()
 {
-    cout<<permut();
+    cout << permut();
 
-    
-
-return 0;
+    return 0;
 }
