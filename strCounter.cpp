4f4ba80aaffb55bfb00b4d5786d398a455a3a041
RusMatvey <92601028+RusMatvey@users.noreply.github.com>
2023-09-26 22:45:16 +0400

    Update strCounter.cpp

--- strCounter.cpp
+++ strCounter.cpp
@@ -1,105 +1,116 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <set>
 
 using namespace std;
 
-struct Char{
-    char in;
-    int type;
+struct Char {
+	char in;
+	int type;
+	bool operator< (const Char& an) const {
+		return in < an.in;
+	}
 };
 
-vector<Char> alf = { {'A', 1}, {'M', 0}, {'F', 0}, {'I', 1}, {'B', 0}, {'R', 0}, {'A', 2}, {'X', 0}, {'I', 2}, {'i', 0} };
+const int WORD_SIZE = 5;
+
+vector<Char> alf = { {'0', 1}, {'1', 1}, {'2', 0}, {'3', 1}, {'4', 0}, {'5', 1}, {'6', 0}, {'7', 1}, {'8', 0} };
 vector<Char> dbug = { {'I', 1}, {'A', 1}, {'I', 2}, {'A', 2}, {'M', 0}, {'F', 0}, {'B', 0}, {'R', 0}, {'X', 0}, {'i', 0} };
 
-int check(vector<Char> a){
-    /*if(a[7] != '0' && a[7] != '5'){
-        return 0;
-    }*/
-    
-    /*if(a[0] == '0'){
-        return 0;
-    }*/
-    
-    for(int i = 0; i < a.size(); ++i){
-        for(int t = i + 1; t < a.size(); ++t){
-            if(a[i].in == a[t].in && a[i].type > a[t].type && a[t].type > 0){
-                return 0;
-            }
-        }
-    }
-    
-    
-    int q_p = 0;
-    
-    for(int i = 0; i < a.size() - 1; ++i){
-        if(a[i].type > 0 && a[i + 1].type > 0){
-            ++q_p;
-        }
-    }
-    if(q_p == 0) return 0;
-    
-    
-    /*if(a[0].in == 'F' || a.back().in == 'F') return 0;
-    
-    for(int i = 1; i < a.size() - 1; ++i){
-        if(a[i].in == 'F'){
-            if(a[i - 1].in != a[i + 1].in || )
-        }
-    }*/
-    
-    
-    
-    return 1;
+int check(vector<Char> a) {
+
+	if(a[0].type == 1 || a.back().in == '1' || a.back().in == '8'){
+		return 0;
+	}
+
+	/*multiset<Char> cont;
+	set<Char> there;
+	for (auto i : a) {
+		cont.insert(i);
+		there.insert(i);
+	}
+	if (cont.count({ '*', 0 }) != 1) return 0;
+	/*int qnc = 0;
+	for (auto i : there) {
+		if (cont.count(i) % 2 == 1) {
+			++qnc;
+		}
+	}
+	if (qnc >= 2) return 0;*/
+	
+
+
+	for (int i = 0; i < a.size(); ++i) {
+		for (int t = i + 1; t < a.size(); ++t) {
+			if (a[i].in == a[t].in && a[i].in == '3') {
+				return 0;
+			}
+		}
+	}
+
+
+	/*for (int i = 0; i < a.size(); ++i) {
+
+	}*/
+
+	/*for(int i = 0; i < a.size() - 1; ++i){
+		int di = 0;
+		while (a[i + di].in == need[di]) ++di;
+		if (di == 10) good = true;
+	}*/
+	
+
+	return 1;
 }
 
 vector<Char> str;
 vector<int> perm;
 
-int permut(){
-    if(perm.size() == 10) {
-        str.clear();
-        for(auto i : perm){
-            str.push_back(alf[i]);
-        }
-        return check(str);
-    }
-    
-    int res = 0;
-    
-    for(int i = 0; i < alf.size(); ++i){
-        
-        for(auto t : perm){
-            if(i == t){
-                goto n_i;
-            }
-        }
-        
-        perm.push_back(i);
-        res += permut();
-        perm.pop_back();
-        
-        n_i:;
-    }
-    
-    return res;
+int permut() {
+	if (perm.size() == WORD_SIZE) {
+		str.clear();
+		for (auto i : perm) {
+			str.push_back(alf[i]);
+		}
+		return check(str);
+	}
+
+	int res = 0;
+
+	for (int i = 0; i < alf.size(); ++i) {
+
+		for (auto t : perm) {
+			if (i == t) {
+				goto n_i;
+			}
+		}
+
+		perm.push_back(i);
+		res += permut();
+		perm.pop_back();
+
+	n_i:;
+	}
+
+	return res;
 }
 
-int silly(){
-    if(str.size() == 5) return check(str);
-    int res = 0;
-    
-    for(auto i : alf){
-        str.push_back(i);
-        res += silly();
-        str.pop_back();
-    }
-    return res;
+int silly() {
+	if (str.size() == WORD_SIZE) return check(str);
+	int res = 0;
+
+	for (auto i : alf) {
+		str.push_back(i);
+		res += silly();
+		str.pop_back();
+	}
+	return res;
 }
 
 int main()
 {
-    cout << permut();
+	cout << silly();
 
-    return 0;
+	return 0;
 }
